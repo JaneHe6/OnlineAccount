f6942bad574403e59a1b1ccf3f84ea0408d186b1
hejiahuan <hejiahuan@roseek.com>
2019-03-18 17:23:47 +0800

    update

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {LIST_VIEW, CHART_VIEW} from './utility';
+import PriceList from './components/PriceList';
+import TotalPrice from './components/TotalPrice';
+import ViewTab from './components/ViewTab';
+import MonthPicker from './components/MonthPicker';
+const items = [
+  {
+    "id":1,
+    "title":"外出旅游",
+    "price":200,
+    "date":"2000-01-01",
+    "category":{
+      "id":"1",
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  },
+  {
+    "id":2,
+    "title":"外出旅游",
+    "price":3000,
+    "date":"2000-01-01",
+    "category":{
+      "id":"1",
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  }
+]
 class App extends Component {
   render() {
     return (
@@ -20,6 +51,23 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        {/* <PriceList 
+          items={items}
+          onModifyItem={(item)=>{alert(item.id)}}
+          onDeleteItem={(item)=>{alert(item.id)}}
+        /> */}
+        {/* <ViewTab 
+          activeTab={LIST_VIEW}
+          onTabChange={(view)=>{console.log(view)}}
+        /> */}
+        {/* <TotalPrice 
+          income={1000}
+          outcome={300}
+        /> */}
+        <MonthPicker 
+          year={2010}
+          month={3}
+        />
       </div>
     );
   }
